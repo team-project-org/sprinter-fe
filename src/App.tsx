d7895af516902b8023ad46583ee57a8a12f6f18a
Teddy (Dongho) <gongdongho12@gmail.com>
2023-12-11 23:13:35 +0900

    feat: 프로젝트 목록 반영

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { CountriesSection } from '@/components/CountriesSection/CountriesSection
 import { SpacexSection } from '@/components/SpacexSection/SpacexSection';
 import { StarWarsSection } from '@/components/StarWarsSection/StarWarsSection';
 import { Button, Card } from 'antd';
-import NavBar from '@/components/NavBar';
-import CommonRouter from '@/CommonRouter';
 import ProjectList from './components/ProjectList';
 
 export const App: React.FC = () => {
   return (
     <div>
-      <ProjectList></ProjectList>
+      <ProjectList />
       <Button type="primary">Button</Button>
       <Card title="Card title" bordered={false} style={{ width: 300 }}>
         <p>Card content</p>
